f9bae72c17d64513325d3296a10c0538b8fd04b1
Kartiksaini001 <kartik100saini@gmail.com>
2021-12-02 13:27:36 +0530

    add ssl check

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import pa11y from "pa11y";
 import cors from "cors";
 import axios from "axios";
+import sslCertificate from "get-ssl-certificate";
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,12 +13,31 @@ app.get("/api", async (req, res) => {
   if (!req.query.url) {
     res.status(400).json({ error: "url is required" });
   } else {
-    const {} = await axios.get(req.query.url);
-    const axiosRes = {};
+    const { headers, status, statusText, request } = await axios.get(
+      req.query.url
+    );
+    let axiosRes = {
+      headers,
+      status,
+      statusText,
+      host: request.host,
+      protocol: request.protocol,
+      ssl: {},
+    };
+
+    // .status .statusText request.host request.protocol=https:
+    // Cookies array headers.server['set-cookie'][i].split(";")
+
     const pallyRes = await pa11y(req.query.url);
 
-    const results = [axiosRes, pallyRes];
-    res.status(200).json(results);
+    sslCertificate.get(axiosRes.host).then((certificate) => {
+      axiosRes.ssl.issuer = certificate.issuer;
+      axiosRes.ssl.valid_from = certificate.valid_from;
+      axiosRes.ssl.valid_to = certificate.valid_to;
+      console.log(axiosRes);
+      const results = [axiosRes, pallyRes];
+      res.status(200).json(results);
+    });
   }
 });
 
